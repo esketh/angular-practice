6c0b0a3b0d8a0713514c18e62814c08b09bb2ba6
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2019-08-02 20:34:11 +0200

    táblázat feltöltése a megadott adatokkal

--- component-interaction-project/src/app/page/bill/bill.component.ts
+++ component-interaction-project/src/app/page/bill/bill.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Bill } from 'src/app/model/bill';
+import { MockData } from 'src/app/model/mock-data';
 
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
   styleUrls: ['./bill.component.css']
 })
+
 export class BillComponent implements OnInit {
   title = "Bills";
+
+  md: MockData = new MockData();
+  bills: Bill[] = this.md.bills;
+
+
   constructor() { }
 
   ngOnInit() {
