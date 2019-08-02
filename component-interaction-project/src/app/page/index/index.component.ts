fedcc38d454159f75b38c2f97f69d421c14ada62
Tamás Takács <tamas.takacs91@gmail.com>
2019-08-02 14:21:36 +0200

    title added to index-component.html

--- component-interaction-project/src/app/page/index/index.component.ts
+++ component-interaction-project/src/app/page/index/index.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  title: string = 'Dashboard'
 
   modalCounter: number = 0;
 
