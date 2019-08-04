133482b942198c5aaec9d7fab3d5542972a178d2
skth <evelin.buth@gmail.com>
2019-08-04 22:21:36 +0200

    fa changes

--- component-interaction-project/src/app/app.module.ts
+++ component-interaction-project/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +26,7 @@ import { ModalComponent } from './modal/modal.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    AngularFontAwesomeModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- component-interaction-project/src/app/page/employee/employee.component.ts
+++ component-interaction-project/src/app/page/employee/employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Employee } from 'src/app/model/employee';
 import { MockData } from 'src/app/model/mock-data';
 
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
