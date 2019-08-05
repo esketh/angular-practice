1707909b82f13342bd325100eaea1f3558b1c7af
skth <evelin.buth@gmail.com>
2019-08-05 13:18:17 +0200

    index added

--- component-interaction-project/src/app/app.module.ts
+++ component-interaction-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { ModalEmpComponent } from './modal-emp/modal-emp.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- component-interaction-project/src/app/page/index/index.component.ts
+++ component-interaction-project/src/app/page/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MockData } from 'src/app/model/mock-data';
 import { Employee } from 'src/app/model/employee';
 import { Bill } from 'src/app/model/bill';
@@ -9,18 +9,22 @@ import { Bill } from 'src/app/model/bill';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  title: string = 'Dashboard';
+
+  data: MockData = new MockData();
+  employeeData: Employee[] = [];
+  billData: Bill[] = [];
 
   modalCounter: number = 0;
-  mockData: MockData = new MockData();
-  employees: Employee[] = [];
-  bills: Bill[] = [];
+
   constructor() {
-    this.employees = this.mockData.employee;
-    this.bills = this.mockData.bills;
-  }
+    this.employeeData = this.data.employee;
+    this.billData = this.data.bills;
+
+  };
 
   ngOnInit() {
-  }
+  };
 
   showModal(): void {
     this.modalCounter++;
