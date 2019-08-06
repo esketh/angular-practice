869fdb3dcaebda8d7f8dc14a19f9e65132c639f7
skth <evelin.buth@gmail.com>
2019-08-06 16:07:48 +0200

    crud

--- data-binding/src/app/service/hero.service.ts
+++ data-binding/src/app/service/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Hero } from '../model/hero';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  jsonUrl: string = "http://localhost:3000/heroes";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+getAll(): Observable<Hero> {
+  return this.http.get<Hero>(this.jsonUrl);
+}
+
 }
