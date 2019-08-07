b3d5b3cf3a29b5eff8d934793d8676cac88fc7df
skth <evelin.buth@gmail.com>
2019-08-07 16:36:16 +0200

    material table

--- http-client-demo/src/app/app.component.ts
+++ http-client-demo/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FilmService } from './service/film.service';
 import { Film } from './model/film';
+import {Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ export class AppComponent implements OnInit {
   title = 'http-client-demo';
   filmList: Film[] = [];
   newFilm: Film = new Film;
+  sortedData: Film[];
 
   constructor(
     private filmService: FilmService
   ) {
-
+    this.sortedData = this.filmList.slice();
   }
 
   ngOnInit() {
@@ -50,4 +52,32 @@ export class AppComponent implements OnInit {
       err => console.error(err)
     )
   }
+
+  sortData(sort: Sort){
+    this.filmService.getAll().subscribe(
+      response => {
+    const data = this.filmList.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+  }
+
+  this.sortedData = data.sort((a, b) => {
+    const isAsc = sort.direction === 'asc';
+    switch (sort.active){
+      case 'title': return compare(a.title, b.title, isAsc);
+      case 'release': return compare(a.release, b.release, isAsc);
+      case 'actor': return compare(a.actor, b.actor, isAsc);
+      default: return 0;
+    }
+    });
+  }
+)
 }
+}
+
+    function compare(a: number | string, b: number | string, isAsc: boolean) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
+
+
--- http-client-demo/src/app/pipe/filter.pipe.ts
+++ http-client-demo/src/app/pipe/filter.pipe.ts
@@ -1,4 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
+import { Film } from '../model/film';
+import { FilmService } from '../service/film.service';
 
 @Pipe({
   name: 'filter'
@@ -7,6 +10,7 @@ export class FilterPipe implements PipeTransform {
 
   transform(baseArray: any, phrase: string = ''): any {
     return baseArray.filter( item => {
+      let jsonString = JSON.stringify(item).replace(/"[^"]*"\:/g, '').replace(/[",\{\}]/g, '');
       return item.title.toLowerCase().indexOf(phrase.toLowerCase()) > -1
     });
   }
