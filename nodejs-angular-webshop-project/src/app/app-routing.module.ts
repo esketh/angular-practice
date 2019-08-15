512ca1b460fb35942cc8d6cbe1037cd82103f941
skth <evelin.buth@gmail.com>
2019-08-15 10:06:05 +0200

    routing

--- nodejs-angular-webshop-project/src/app/app-routing.module.ts
+++ nodejs-angular-webshop-project/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './page/index/index.component';
+import { AdminComponent} from './page/admin/admin.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "", component: IndexComponent},
+  {path: "admin",   component: AdminComponent},
+  {path: "**", component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- nodejs-angular-webshop-project/src/app/app.module.ts
+++ nodejs-angular-webshop-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './page/index/index.component';
+import { AdminComponent } from './page/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
