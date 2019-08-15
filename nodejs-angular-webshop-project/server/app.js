b99b12f9c980eb31f2baeecfa87d9d0be4e22d52
skth <evelin.buth@gmail.com>
2019-08-15 15:37:14 +0200

    gyakorlas

---
+++ nodejs-angular-webshop-project/server/app.js
@@ -0,0 +1,19 @@
+/* eslint-disable keyword-spacing */
+const http = require('http');
+const path = require('path');
+const port = 3210;
+const GetHandler = require('./module/getHandler');
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  switch(req.method.toLowerCase()) {
+    case 'get': new GetHandler(req, res);
+      break;
+    default: res.end('invalid method');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port: ${port}.`);
+});
--- nodejs-angular-webshop-project/src/app/page/admin/admin.component.ts
+++ nodejs-angular-webshop-project/src/app/page/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/service/order.service';
+import { Order } from 'src/app/model/order';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  list: Order[] = [];
+  list$: Observable<any> = this.orderService.getAll() 
+  // a htmlben async pipe-pal kell csinálni
+
+  constructor(
+    private orderService: OrderService
+  ) { }
 
   ngOnInit() {
+    // this.orderService.getAll().subscribe(
+    //   orders => this.list = orders,
+    //   err => console.error(err)
+    // );
   }
 
 }
